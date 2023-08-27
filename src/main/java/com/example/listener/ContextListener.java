121f4d18fecbd5bdb62372108c7ef42d0c7c34e5
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-08-27 13:06:44 +0300

    Servlets/Filter/Session

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,15 @@
 package com.example.listener;
 
-public class ContextListener {
-    //write your code here!
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+    public void contextInitialized(ServletContextEvent event){
+        LocalDateTime time = LocalDateTime.now();
+        event.getServletContext().setAttribute("servletTimeInit",time);
+    }
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,45 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import com.example.Users;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user")==null) {
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
+        }
+        else{
+            response.sendRedirect(request.getContextPath() + "/user/hello.jsp");
+        }
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        HttpSession session = request.getSession();
+        String login = request.getParameter("login");
+        boolean isPasswordPresent = !(request.getParameter("password").isEmpty());
+        int loginFlag=0;
+        List<String> users = Users.getInstance().getUsers();
+        for (String logins:users) {
+            if(login.equals(logins)) {
+                loginFlag=1;
+                break;
+            }
+        }
+        if(loginFlag==1 && isPasswordPresent==true){
+            session.setAttribute("user", login);
+            response.sendRedirect(request.getContextPath() + "/user/hello.jsp");
+        }
+        else{
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }
+    }
 }
